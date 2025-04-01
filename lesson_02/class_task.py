368b8952b7010cacf822c5611a992c5c550125a5
AyalaDruk <ayalahorberg1999@gmail.com>
2025-04-01 21:56:02 +0300

    add integer

---
+++ lesson_02/class_task.py
@@ -0,0 +1,14 @@
+num = input("enter a number")
+num1=int(num)
+print(num1*2)
+print(num1/2)
+
+x =int(input("enter a height"))
+y= int(input("enter a width"))
+s= x+y
+p=2*(x+y)
+print(s,p)
+
+f = int(input("enter a temp"))
+c =(f-32)*5/9
+print(c)
